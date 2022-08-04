3af9a9018d62c9d4fb05de24cd2dc3a071fe7de3
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-08-04 13:03:06 -0400

    Set up navigation bar and routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import Directory from './components/directory/directory';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home';
+import Navigation from './routes/navigation/navigation';
 import './App.css';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Weapons',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'Armors',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'Accessories',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'Supplies',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'Decorations',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<h1>Leave now!</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
