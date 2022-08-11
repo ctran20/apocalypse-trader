234c2d830b08a63055f7bfbe7c53bd4ff5105264
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-08-11 19:19:22 -0400

    Added Cart component and context

--- src/contexts/products.context.js
+++ src/contexts/products.context.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 import PRODUCTS from '../shopdata.json';
 
 // Actual value for accessing
--- src/routes/navigation/navigation.js
+++ src/routes/navigation/navigation.js
@@ -2,12 +2,16 @@ import { Outlet, Link } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
+import CartIcon from '../../components/cart-icon/cart-icon';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -28,7 +32,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropDown />}
       </div>
       <Outlet />
     </Fragment>
